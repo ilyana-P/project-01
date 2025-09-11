b1adba6f375e6045bf0bcf5c034d298946d6343f
Ilyana Potnyaeva <il.potnyaeva@gmail.com>
2025-09-11 20:40:38 +0200

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import './App.css'
-import DogCard from './components/DogCard/DogCard'
-import Goodbye from './components/Goodbye/Goodbye'
-import Greeting from './components/Greeting/Greeting'
-import ProfileCard from './components/ProfileCard/ProfileCard'
+import "./App.css";
+import Counter from "./components/Counter/Counter";
+import DogCard from "./components/DogCard/DogCard";
+import Goodbye from "./components/Goodbye/Goodbye";
+import Greeting from "./components/Greeting/Greeting";
+import ProfileCard from "./components/ProfileCard/ProfileCard";
 
 function App() {
   return (
     <>
-    <p> Hello,React!</p>
-     <Greeting/>
-     <Greeting/>
-     <Greeting/>
-     <Goodbye name={"Bob"}/>
-     <Goodbye name={"John"}/>
-     <DogCard/>
-     <ProfileCard
-     avatar="https://media.istockphoto.com/id/458587791/de/foto/fremder-tides-poster-auf.jpg?s=612x612&w=is&k=20&c=MA6dH_6ZKmrGuO_BOXkM_cGWw9T3J0uAfY7WXl0phrI="
-     name= "Captain Jack Sparrow"
-     description= "Pirate Lord of the Caribbean sea"
-     />
+      <p> Hello,React!</p>
+      <Greeting />
+      <Counter />
+      <Goodbye name={"Bob"} />
+      <Goodbye name={"John"} />
+      <DogCard />
+      <ProfileCard
+        avatar="https://media.istockphoto.com/id/458587791/de/foto/fremder-tides-poster-auf.jpg?s=612x612&w=is&k=20&c=MA6dH_6ZKmrGuO_BOXkM_cGWw9T3J0uAfY7WXl0phrI="
+        name="Captain Jack Sparrow"
+        description="Pirate Lord of the Caribbean sea"
+      />
     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [counter, setCounter] = useState(10);
+  // переменная состояния == state
+  // функция setter - вызвывать ререндер компонента
+  // Что нужно запомнить о каждом хук?
+  // - что принимает? Начальное значение переменной состояния.
+  // - что возвращает? Создает стейт, изменения которго вызывают ререндер
+
+  function handleAddOne() {
+    setCounter(counter + 1);
+  }
+  function handleMinusTwo() {
+    // setCounter(counter - 2);
+    setCounter((prev) => prev - 2);
+  }
+
+  function handleResetCounter() {
+    setCounter(0);
+  }
+
+  //   СОЗДАЙТЕ КНОПКУ -2
+
+  return (
+    <div>
+      <h2>{counter}</h2>
+
+      <button type="button" onClick={handleAddOne}>
+        +1
+      </button>
+
+      <button type="button" onClick={handleMinusTwo}>
+        -2
+      </button>
+
+      <button type="button" onClick={handleResetCounter}>
+        Reset
+      </button>
+    </div>
+  );
+}
